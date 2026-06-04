3bfcc90d6bbb3e180600f0ed80c8dfff8b3094bb
dab1213 <popaorlov@gmail.com>
2026-06-04 21:24:42 +0400

    add UserDetailsDto for user authentication and refactor UserDto; implement user registration in UserService

---
+++ src/main/java/org/example/cloudstorage/dto/UserDetailsDto.java
@@ -0,0 +1,32 @@
+package org.example.cloudstorage.dto;
+
+import lombok.AllArgsConstructor;
+import org.example.cloudstorage.entity.Role;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserDetailsDto implements UserDetails {
+    private String username;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
--- src/main/java/org/example/cloudstorage/service/UserService.java
+++ src/main/java/org/example/cloudstorage/service/UserService.java
@@ -1,11 +1,15 @@
 package org.example.cloudstorage.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.cloudstorage.dto.UserDetailsDto;
 import org.example.cloudstorage.dto.UserDto;
+import org.example.cloudstorage.entity.Role;
+import org.example.cloudstorage.entity.User;
 import org.example.cloudstorage.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,11 +17,23 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registration(UserDto userDto) {
+
+        var user = User.builder()
+                .username(userDto.username())
+                .password(passwordEncoder.encode(userDto.password()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
-                .map(user -> new UserDto(
+                .map(user -> new UserDetailsDto(
                         user.getUsername(),
                         user.getPassword(),
                         user.getRole()
