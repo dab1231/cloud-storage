bd47714b9e43329f1fc34aa24dd20c80f9b3346a
dab1213 <popaorlov@gmail.com>
2026-06-05 15:46:19 +0400

    implement user authentication with sign-in endpoint and update global exception handling

--- src/main/java/org/example/cloudstorage/controller/AuthController.java
+++ src/main/java/org/example/cloudstorage/controller/AuthController.java
@@ -7,6 +7,10 @@ import org.example.cloudstorage.dto.response.UserResponse;
 import org.example.cloudstorage.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/sign-up")
     public ResponseEntity<UserResponse> registration(@RequestBody UserRequest userRequest,
                                                      HttpServletRequest request) {
 
         var user = userService.registration(userRequest);
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(
+                userRequest.username(), userRequest.password());
+
+        Authentication authenticationResponse = authenticationManager.authenticate(authenticationRequest);
+        SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
         request.getSession(true);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new UserResponse(user.username()));
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<UserResponse> login(@RequestBody UserRequest userRequest, HttpServletRequest request) {
+
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(
+                userRequest.username(), userRequest.password());
+
+        Authentication authenticationResponse = authenticationManager.authenticate(authenticationRequest);
+        SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
+        request.getSession(true);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new UserResponse(authenticationResponse.getName()));
     }
 }
--- src/main/java/org/example/cloudstorage/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/cloudstorage/exception/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package org.example.cloudstorage.exception;
 import org.example.cloudstorage.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
