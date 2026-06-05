43792a0b968f39f053beb5b5446b9b40f9b7cc41
dab1213 <popaorlov@gmail.com>
2026-06-05 15:55:35 +0400

    add exception handler for AuthenticationException in GlobalExceptionHandler

--- src/main/java/org/example/cloudstorage/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/cloudstorage/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.example.cloudstorage.exception;
 import org.example.cloudstorage.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,4 +16,13 @@ public class GlobalExceptionHandler {
         var errorMessage = e.getMessage();
         return new ResponseEntity<>(new ErrorResponse(errorMessage), HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+
+        var errorMessage = e.getMessage();
+        return new ResponseEntity<>(new ErrorResponse(errorMessage), HttpStatus.UNAUTHORIZED);
+    }
+
+
 }
