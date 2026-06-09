7c16fe71885b796835a047e27da911d34fe832c0
dab1213 <popaorlov@gmail.com>
2026-06-09 13:44:54 +0400

    add integration tests for UserService with PostgreSQL using Testcontainers

---
+++ src/test/java/org/example/cloudstorage/integration/service/UserServiceTest.java
@@ -0,0 +1,68 @@
+package org.example.cloudstorage.integration.service;
+
+import org.example.cloudstorage.dto.request.UserRequest;
+import org.example.cloudstorage.repository.UserRepository;
+import org.example.cloudstorage.service.UserService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class UserServiceTest {
+
+    final String USERNAME = "test";
+    final String PASSWORD = "test";
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+        "postgres:16"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgres::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgres::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void registration() {
+        var user = new UserRequest(USERNAME, PASSWORD);
+
+        var userResponse = userService.registration(user);
+
+        var actualUser = userRepository.findByUsername(userResponse.username());
+
+        assertTrue(actualUser.isPresent());
+        actualUser.ifPresent(actual -> assertEquals(USERNAME, actual.getUsername()));
+    }
+}
