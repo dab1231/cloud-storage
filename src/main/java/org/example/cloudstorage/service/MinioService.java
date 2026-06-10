290d2134bef83987143cc6c9e15dbeca3d5158b1
dab1213 <popaorlov@gmail.com>
2026-06-10 13:08:32 +0400

    add deleteResource method and improve path validation in MinioService

--- src/main/java/org/example/cloudstorage/service/MinioService.java
+++ src/main/java/org/example/cloudstorage/service/MinioService.java
@@ -1,29 +1,35 @@
 package org.example.cloudstorage.service;
 
-import io.minio.MinioClient;
-import io.minio.StatObjectArgs;
+import io.minio.*;
 import io.minio.errors.MinioException;
+import io.minio.messages.DeleteRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.cloudstorage.dto.response.DirectoryResponse;
 import org.example.cloudstorage.dto.response.FileResponse;
 import org.example.cloudstorage.dto.response.ResourceResponse;
 import org.example.cloudstorage.exception.InvalidPathException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class MinioService {
 
+    @Value("${minio.bucket-name}")
+    private String bucketName;
     private final MinioClient minioClient;
 
     public ResourceResponse getInfo(String path) throws MinioException {
 
-        if (path.isBlank() || path.contains("..")) {
-            throw new InvalidPathException("Path must not be blank or contain '..'");
-        }
+        ifPathInvalidThrowException(path);
 
         var statObjectResponse = minioClient.statObject(StatObjectArgs.builder()
-                .bucket("user-files")
+                .bucket(bucketName)
                 .object(path)
                 .build());
 
@@ -42,6 +48,46 @@ public class MinioService {
             );
         }
 
+    }
+
+    public void deleteResource(String path) throws MinioException {
+
+        ifPathInvalidThrowException(path);
+
+        if (path.endsWith("/")) {
+            var results = minioClient.listObjects(ListObjectsArgs.builder()
+                    .bucket(bucketName)
+                    .prefix(path)
+                    .recursive(true)
+                    .build());
+
+            List<DeleteRequest.Object> deleteList = new ArrayList<>();
+            for (var itemResult : results) {
+                deleteList.add(new DeleteRequest.Object(itemResult.get().objectName()));
+            }
+
+            var deleteResult = minioClient.removeObjects(RemoveObjectsArgs.builder()
+                    .bucket(bucketName)
+                    .objects(deleteList)
+                    .build());
+
+            for (var error : deleteResult) {
+                log.warn(error.get().message());
+            }
+
+        } else {
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(path)
+                    .build());
+        }
+
+    }
+
+    private static void ifPathInvalidThrowException(String path) {
+        if (path.isBlank() || path.contains("..")) {
+            throw new InvalidPathException("Path must not be blank or contain '..'");
+        }
     }
 
     private String getName(String path) {
